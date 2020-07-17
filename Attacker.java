4ea812ca37200be4650cdeab217cb211616a1dd0
GideonO224 <67971236+GideonO224@users.noreply.github.com>
2020-07-17 15:09:24 -0700

    Update Attacker.java

--- Attacker.java
+++ Attacker.java
@@ -8,20 +8,8 @@
 public interface Attacker
 {
      public void attack(Animal prey);
-     /*{
-          if(prey.health > 0)
-          {
-               System.out.println("Attacking");
-               prey.hit();
-          }
-          if(prey.health == 0)
-          {
-               count++;
-          }
-     }*/
+     
      public int getKillCount();
-     /*{
-          return count;
-     }*/
+     
      
 }
