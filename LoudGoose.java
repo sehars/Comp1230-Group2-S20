b5fffb87a3ecdd08cfd5632b86a88c29b6e78152
GideonO224 <67971236+GideonO224@users.noreply.github.com>
2020-07-17 15:16:15 -0700

    Update LoudGoose.java

--- LoudGoose.java
+++ LoudGoose.java
@@ -4,10 +4,28 @@
 // Assignment 4
 //********************************************************************
 
-public class LoudGoose extends WaterFowl         
+public class LoudGoose extends WaterFowl implements Attacker
 {
      public LoudGoose(String name, String color)
      {
           super(name, color);
-     } 
+          int count
+     }
+     
+     public void attack(Animal prey);
+     {
+          if(prey.health > 0)
+          {
+               System.out.println("Attacking");
+               prey.hit();
+          }
+          if(prey.health == 0)
+          {
+               count++;
+          }
+     }
+     public int getKillCount();
+     /*{
+          return count;
+     }*/
 }
